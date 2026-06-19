9e1867ee483673aaa5ed81c585baf10c27a4aef5
World <parsa.jahangir2007@gmail.com>
2026-06-20 00:09:26 +0330

    feat: create dice to start the Game Engin at Controller Package

---
+++ src/ir/ac/um/siliconvalley/model/Dice.java
@@ -0,0 +1,24 @@
+package ir.ac.um.siliconvalley.model;
+
+import java.util.Random;
+
+public class Dice {
+
+    private int currentFace;
+    private final Random random;
+
+
+    public Dice() {
+
+        this.currentFace = 0;
+        this.random =new Random();
+
+    }
+
+    public int roll() {
+
+        this.currentFace = random.nextInt(6)+1;
+        return this.currentFace;
+
+    }
+}
