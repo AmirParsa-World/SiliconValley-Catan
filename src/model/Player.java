6aaf9c563411533ec18ba805d54a2080845891e1
World <parsa.jahangir2007@gmail.com>
2026-07-07 12:16:37 +0330

    The player class was modified by both codes.

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,8 +1,10 @@
 package model;
 
 import exception.NotEnoughResourceException;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 public class Player {
 
@@ -12,62 +14,67 @@ public class Player {
     private final Map<ResourceType, Integer> wallet;
     private final List<Structure> structures;
 
-
-    public Player (String name, String color) {
-
+    public Player(String name, String color) {
         this.name = name;
         this.color = color;
         this.role = null;
-
         this.structures = new ArrayList<>();
-        this.wallet = new EnumMap<>(ResourceType.class); // now we created an object from Map class
-                                       //( it's not the map(board) of the game)
+
+        // Enum map: baran
+        this.wallet = new EnumMap<>(ResourceType.class);
 
         for (ResourceType type : ResourceType.values()) {
             this.wallet.put(type, 0);
         }
-
     }
 
+    // getter methods: world
+    public String getName() { return name; }
+    public String getColor() { return color; }
+    public List<Structure> getStructures() { return structures; }
+    public Map<ResourceType, Integer> getWallet() { return wallet; }
 
-    public void addResource (ResourceType type, int amount) {
-
-        int currentAmount = wallet.get(type);
-        this.wallet.put(type, amount+currentAmount);
-
+    // Some other good methods: baran
+    public int getResource(ResourceType type) {
+        return wallet.getOrDefault(type, 0);
     }
 
-    public void spendResource (ResourceType type, int amount) {
+    // baran
+    public int getTotalResources() {
+        int total = 0;
+        for (int amount : wallet.values()) {
+            total += amount;
+        }
+        return total;
+    }
 
-        if (wallet.get(type) < amount )
-            throw new NotEnoughResourceException("Dear player, your haven't enough" + type + " resource ");
+    public void addResource(ResourceType type, int amount) {
+        int currentAmount = wallet.getOrDefault(type, 0);
+        this.wallet.put(type, amount + currentAmount);
+    }
 
+    public void spendResource(ResourceType type, int amount) {
+        if (wallet.getOrDefault(type, 0) < amount) {
+            throw new NotEnoughResourceException("Dear player, you don't have enough " + type + " resource.");
+        }
         this.wallet.put(type, wallet.get(type) - amount);
-
-
     }
 
-    //TODO: give 1 point for the longest partnership (at least must be 3 partnerships)
+    public void addStructure(Structure structure) {
+        structures.add(structure);
+    }
 
+    // world
     public int countPlayerPoint() {
-
-        int totalPoint = (this.role != null) ? -1 : 0; // shorter and easier.
-//        int totalPoints;
-//        if (this.role != null) {
-//            totalPoints = -1;
-//        } else {
-//            totalPoints = 0;
-//        }
-
-        for (Structure structure : this.structures) { // we handle the outOfBoundException by writing this form.
+        int totalPoint = (this.role != null) ? -1 : 0;
+        for (Structure structure : this.structures) {
             totalPoint += structure.getPoint();
         }
-
         return totalPoint;
     }
 
-// Finish TODO.
-
-
-
+    @Override
+    public String toString() {
+        return name + " (" + color + ") | Points: " + countPlayerPoint() + " | Total Resources: " + getTotalResources();
+    }
 }
