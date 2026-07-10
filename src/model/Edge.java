3c3637c00f8e6d723c8010eb5c07eb9c77169e03
World <parsa.jahangir2007@gmail.com>
2026-07-11 01:04:39 +0330

    Redesign Edge constructor to accept connecting vertices.

--- src/model/Edge.java
+++ src/model/Edge.java
@@ -1,25 +1,24 @@
 package model;
 
 public class Edge {
-
+    private final Vertex u; // Connecting intersection 1
+    private final Vertex v; // Connecting intersection 2
     private Player owner;
-
     private boolean partnership;
 
-
-    public Edge(Player owner,
-                boolean partnership) {
-
-        this.owner = owner;
-        this.partnership = partnership;
+    // The correct constructor for map initialization!
+    public Edge(Vertex u, Vertex v) {
+        this.u = u;
+        this.v = v;
+        this.owner = null;        // No owner at start of game
+        this.partnership = false; // No partnership built yet
     }
 
-    public Player getOwner() {
-        return owner;
-    }
+    public Vertex getU() { return u; }
+    public Vertex getV() { return v; }
+    public Player getOwner() { return owner; }
+    public boolean isPartnership() { return partnership; }
 
-    public boolean isPartnership() {
-        return partnership;
-    }
-
+    public void setOwner(Player owner) { this.owner = owner; }
+    public void setPartnership(boolean partnership) { this.partnership = partnership; }
 }
